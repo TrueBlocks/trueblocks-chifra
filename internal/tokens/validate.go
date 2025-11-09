c2f68a4036b4dfa88bd27a15de09181901aad392
Thomas Jay Rush <jrush@quickblocks.io>
2025-11-08 21:47:11 -0500

    Allows for tokens --approvals without first having done chifra list

--- internal/tokens/validate.go
+++ internal/tokens/validate.go
@@ -6,12 +6,9 @@ package tokensPkg
 
 import (
 	"errors"
-	"path/filepath"
 
 	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/base"
 	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/config"
-	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/file"
-	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/monitor"
 	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/rpc"
 	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/validate"
 )
@@ -45,13 +42,6 @@ func (opts *TokensOptions) validateTokens() error {
 
 		if len(opts.Addrs) == 0 {
 			return validate.Usage("You must specify at least one address")
-		}
-
-		for _, addr := range opts.Addrs {
-			path := filepath.Join(config.PathToCache(opts.Globals.Chain), "monitors", addr+monitor.Ext)
-			if !file.FileExists(path) {
-				return validate.Usage("Run `chifra list {1}` before querying approvals.", addr, addr)
-			}
 		}
 
 	} else {
